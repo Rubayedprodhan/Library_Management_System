2e23c683e43428e7a80dc531fc758674ea75169c
Rubayedprodhan <rubayedprodhan550@gmail.com>
2024-09-22 23:20:22 +0600

    added

--- Library_management/settings.py
+++ Library_management/settings.py
@@ -79,23 +79,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Library_management.wsgi.application'
 
-# Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       "NAME": env("DB_NAME"),
-       "USER": env("DB_USER"),
-       "PASSWORD": env("DB_PASSWORD"),
-       'HOST': env("DB_HOST"),
-       'PORT': env("DB_PORT"), 
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        "NAME": env("DB_NAME"),
+#        "USER": env("DB_USER"),
+#        "PASSWORD": env("DB_PASSWORD"),
+#        'HOST': env("DB_HOST"),
+#        'PORT': env("DB_PORT"), 
+#    }
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
